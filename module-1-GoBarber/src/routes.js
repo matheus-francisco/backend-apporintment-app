fcf13873c0aedc6ffd6e3a51020d7f8d5422b313
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-07-09 10:46:10 -0300

    finish user and file upload

--- module-1-GoBarber/src/routes.js
+++ module-1-GoBarber/src/routes.js
@@ -1,15 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
+
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
