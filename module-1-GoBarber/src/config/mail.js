9b5cdecf48611143ab35b02c3cfd38e857f1755c
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-07-09 14:22:37 -0300

    confi mongodb and appointments notifications, and send email

---
+++ module-1-GoBarber/src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3f51bd9d416e85',
+    pass: '4900e9a336fa3a',
+  },
+  default: {
+    from: 'Team GoBarber <noreplay@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// Gamil
+// Mailtrap (ambiente de DEV)
--- module-1-GoBarber/src/routes.js
+++ module-1-GoBarber/src/routes.js
@@ -9,6 +9,8 @@ import ProviderController from './app/controller/ProviderController';
 
 import authMiddlewares from './app/middlewares/auth';
 import AppointmentController from './app/controller/AppointmentController';
+import ScheduleController from './app/controller/ScheduleController';
+import NotificationController from './app/controller/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,6 +25,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.post('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
